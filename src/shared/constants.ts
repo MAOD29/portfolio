736d26e2bf3de1e53967c53efd46702c1ed232d9
MAOD29 <miguel_olea29@outlook.com>
2023-06-12 17:48:09 -0600

    Init proyects section

---
+++ src/shared/constants.ts
@@ -0,0 +1,107 @@
+export const PROYECTS = [
+  {
+    title: "ROCK PAPER SCISSORS",
+    src: "/proyect_rps.webp",
+    github: "https://github.com/MAOD29/rock-paper-scissors",
+    demo: "https://rps-game-online.netlify.app/",
+  },
+  {
+    title: "TODO LIST",
+    src: "/proyect_todo.webp",
+    github: "https://github.com/MAOD29/todo-list",
+    demo: "https://my-todo-list-online.netlify.app/",
+  },
+  {
+    title: "ROCK PAPER SCISSORS",
+    src: "/proyect_rps.webp",
+    github: "https://github.com/MAOD29/rock-paper-scissors",
+    demo: "https://rps-game-online.netlify.app/",
+  },
+  {
+    title: "TODO LIST",
+    src: "/proyect_todo.webp",
+    github: "https://github.com/MAOD29/todo-list",
+    demo: "https://my-todo-list-online.netlify.app/",
+  },
+];
+
+export const LINKS = [
+  {
+    src: "https://github.com/MAOD29",
+    svg: "/github.svg",
+    description: "",
+  },
+  {
+    src: "https://www.linkedin.com/in/maod/",
+    svg: "/linkedin.svg",
+    description: "",
+  },
+  {
+    src: "https://www.linkedin.com/in/maod/overlay/1635501886583/single-media-viewer/?profileId=ACoAACq4pAQBjs9AQH5ziJShgI-FfNL7aT7_bF0",
+    svg: "/cv.svg",
+    description: "Revisa mi CV ",
+  },
+];
+
+export const SKILLS = [
+  {
+    src: "/js.svg",
+    alt: "JavaScript",
+    name: "JavaScript",
+  },
+  {
+    src: "/ts.svg",
+    alt: "TypeScript",
+    name: "TypeScript",
+  },
+  {
+    src: "/react.svg",
+    alt: "React",
+    name: "React",
+  },
+  {
+    src: "/next.svg",
+    alt: "Next",
+    name: "Next",
+  },
+  {
+    src: "/vue.svg",
+    alt: "Vue",
+    name: "Vue",
+  },
+  {
+    src: "/nuxt.svg",
+    alt: "Nuxt",
+    name: "Nuxt",
+  },
+  {
+    src: "/angular.svg",
+    alt: "Angular",
+    name: "Angular",
+  },
+  {
+    src: "/astro.svg",
+    alt: "Astro",
+    name: "Astro",
+  },
+  {
+    src: "/sass.svg",
+    alt: "Sass",
+    name: "Sass",
+  },
+  {
+    src: "/tailwind.svg",
+    alt: "tailwindcss",
+    name: "tailwindcss",
+  },
+  {
+    src: "/vitest.svg",
+    alt: "Vitest",
+    name: "Vitest",
+  },
+  {
+    src: "/git.svg",
+    alt: "Git",
+    name: "Git",
+  },
+];
